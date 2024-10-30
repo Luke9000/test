7fcec1f7e7edeca6640647126938e6918c21e401
Luke9000 <rampel227@gmail.com>
2024-10-30 18:22:44 +0300

    0.23

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/test/',
+  base: '/test',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
